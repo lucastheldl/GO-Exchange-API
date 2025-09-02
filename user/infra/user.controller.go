38ea44e5ae4df0a1c992adfd4117effd02ae49f0
lucas Emanoel <lucastheldl@gmail.com>
2025-09-02 14:25:36 -0300

    feat: added-login-controller

--- user/infra/user.controller.go
+++ user/infra/user.controller.go
@@ -16,6 +16,7 @@ import (
 func RegisterRoutes(router *mux.Router, conn *pgx.Conn) {
 	handler := &UserHandler{conn: conn}
 	router.HandleFunc("/signup", handler.register).Methods("POST")
+	router.HandleFunc("/signin", handler.login).Methods("POST")
 }
 
 type UserHandler struct {
@@ -31,6 +32,22 @@ func (h *UserHandler) register(w http.ResponseWriter, r *http.Request){
 	repo := NewUserRepo(h.conn)
 
 	user,err := application.RegisterUserUseCase(ctx,input,repo)
+
+	if err != nil{
+		http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+func (h *UserHandler) login(w http.ResponseWriter, r *http.Request){
+	var input domain.UserInput
+
+
+	ctx := context.Background()
+	repo := NewUserRepo(h.conn)
+
+	user,err := application.LoginUserUseCase(ctx,input,repo)
 
 	if err != nil{
 		http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
